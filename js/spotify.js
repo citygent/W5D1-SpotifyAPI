1777089c2d71087e9a4cc65992090c8dda681807
James White <cyberyt@gmail.com>
2015-09-07 17:59:39 +0100

    DOM values > JS

--- js/spotify.js
+++ js/spotify.js
@@ -3,10 +3,18 @@ $(document).ready(function() {
 
 var searchButton = $('#get-results')
 var searchSelect = $('#filter')
-var searchInput = $('#search-input')
+var searchBox = $('#search-input')
 var results = $('#results')
 
+searchButton.on('click', doThing);
+searchSelect.on('change', doThing);
 
+function doThing(e) {
+  searchInput = searchBox.val()
+  searchFilter = searchSelect.val()
+  console.log(searchInput);
+  console.log(searchFilter);
+}
 
 
 
