14d5ee3cd6df490dfab45e466ea5d36ddb7b95bf
James White <cyberyt@gmail.com>
2015-09-07 17:38:44 +0100

    Laid down some sweet HTML

---
+++ js/spotify.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  console.log('Merry-go-round Breaks Down')
+
+var searchButton = $('#get-results')
+var searchSelect = $('#filter')
+var searchInput = $('#search-input')
+var results = $('#results')
+
+
+
+
+
+
+
+
+});
+
